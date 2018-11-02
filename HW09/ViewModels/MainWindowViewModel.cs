73a46705ad270bdd299bc4be979fae70ceb3d606
Michael Lewellen <michael.lewellen@snow.edu>
2018-11-02 08:32:50 -0600

    It is just the bones, I haven't implimented the Open Interface

--- HW09/ViewModels/MainWindowViewModel.cs
+++ HW09/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace HW09.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ViewModelBase , INotifyPropertyChanged
     {
-        public string Greeting => "Hello World!";
+        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
